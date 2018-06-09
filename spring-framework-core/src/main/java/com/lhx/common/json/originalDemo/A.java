444472822690d5f072142d751491607b5c3fe3b9
bjlhx15 <bjlhx15@163.com>
2018-06-09 22:58:48 +0800

    init

---
+++ spring-framework-core/src/main/java/com/lhx/common/json/originalDemo/A.java
@@ -0,0 +1,13 @@
+package com.lhx.common.json.originalDemo;
+
+public class A {
+	private String name;
+
+	public String getName() {
+		return this.name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
