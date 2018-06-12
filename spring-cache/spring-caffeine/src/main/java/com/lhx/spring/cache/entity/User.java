2cbfa32646dfa7e65a12fcdaa56b5656a6645807
bjlhx15 <bjlhx15@163.com>
2018-06-12 22:52:18 +0800

    cache

---
+++ spring-cache/spring-caffeine/src/main/java/com/lhx/spring/cache/entity/User.java
@@ -0,0 +1,35 @@
+package com.lhx.spring.cache.entity;
+
+import java.io.Serializable;
+
+public class User implements Serializable {
+	private String name;
+	private int age;
+
+	public User() {
+		super();
+	}
+
+	public User(String name, int age) {
+		super();
+		this.name = name;
+		this.age = age;
+	}
+
+	public String getName() {
+		return this.name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return this.age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+}
---
+++ spring-cache/spring-caffeine/src/main/java/com/lhx/spring/cache/services/IUserService.java
@@ -0,0 +1,11 @@
+package com.lhx.spring.cache.services;
+
+import com.lhx.spring.cache.entity.User;
+
+public interface IUserService {
+	User queryUser(String id);
+
+	User updateUser(String id);
+
+	int deleteUser(String id);
+}
